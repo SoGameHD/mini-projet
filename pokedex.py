fc219ba832ba4ad22cd75a9eaea6ed55befc7a6f
Mathurin RATIE <mathurin.ratie@epsi.fr>
2022-11-04 16:21:38 +0100

    Print Pokemon names

--- pokedex.py
+++ pokedex.py
@@ -1,6 +1,17 @@
 import requests
+from tkinter import *
 
 api_url = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
 data = requests.get(api_url).json()
 
-print(len(data["results"]))
+window=Tk()
+
+for i in range(151):
+    colonne = (i-i%25)/25
+    lbl=Label(window, text=(str(i+1) + " - " + data["results"][i]["name"]), font=("TimesNewRoman", 16))
+    lbl.place(x=(colonne*200)+50, y=32*(i%25))
+
+
+window.title('Mon Pokedex')
+window.geometry("1600x800+10+20")
+window.mainloop()
